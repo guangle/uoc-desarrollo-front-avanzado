9405b7dca14308a625856d198a520d15c434d3c6
Juan Jesús Gutiérrez Ramos <guangle@gmail.com>
2020-03-04 11:28:27 +0100

    maquetamos formulario
    maquetamos el formulario..

--- pec1/UOCJob9/src/app/views/edit-profile/edit-profile.component.ts
+++ pec1/UOCJob9/src/app/views/edit-profile/edit-profile.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from "../../shared/services/user.service";
 import { User } from "../../shared/models/user.model";
 import { Router } from "@angular/router";
+import { FormControl, FormGroup } from "@angular/forms";
+import { Validators, FormBuilder } from "@angular/forms";
 
 @Component({
   selector: 'app-edit-profile',
@@ -11,12 +13,31 @@ import { Router } from "@angular/router";
 export class EditProfileComponent implements OnInit {
 
   public user: User;
+  public editProfileForm: FormGroup;
 
-  constructor(private userService: UserService, private router: Router) {
+  constructor(private userService: UserService, private router: Router, private fb: FormBuilder) {
     this.user = this.userService.user;
+    this.createForm();
   }
 
   ngOnInit(): void {
+  }
+
+  editProfile() {
+    console.log('Edit profile: submit');
+  }
+
+  createForm() {
+    console.log('Creando el formulario de edición de perfil');
+    this.editProfileForm = this.fb.group({
+      /*
+      email: [
+        null,
+        [Validators.required, Validators.minLength(10), Validators.email]
+      ],
+      password: [null, [Validators.required, Validators.minLength(4)]]
+      */
+    });
   }
 
 }
--- pec1/UOCJob9/src/app/views/edit-profile/edit-profile.module.ts
+++ pec1/UOCJob9/src/app/views/edit-profile/edit-profile.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from "@angular/common";
 
 import { EditProfileRoutingModule } from "./edit-profile-routing.module";
 import { EditProfileComponent } from "./edit-profile.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
   declarations: [EditProfileComponent],
-  imports: [CommonModule, EditProfileRoutingModule]
+  imports: [CommonModule, EditProfileRoutingModule, ReactiveFormsModule]
 })
 export class EditProfileModule {}
